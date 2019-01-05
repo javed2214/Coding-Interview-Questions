779168baa7c77a3b016d552be6962e0f76aa86b3
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-05 19:56:33 +0530

    Add files via upload

---
+++ r21.cpp
@@ -0,0 +1,28 @@
+// Program to Count Occurrence of Substring in Main String
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	string text="acbd";
+	string pat="cb";
+	int n1=text.length();
+	int n2=pat.length();
+	int x=n1-n2;
+	int ans=0;
+	bool found;
+	
+	for(int i=0;i<=x;i++){
+		found=true;
+		for(int j=i;j<=i+n2-1;j++){
+			if(pat[j-i]!=text[j])
+				found=false;
+		}
+		if(found)
+			ans++;
+	}
+	cout<<ans;
+
+	return 0;
+}
