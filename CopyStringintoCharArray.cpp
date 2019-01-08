fa1545b5fcf8e36e3f0fcb3674b2fac010a3304b
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-09 02:00:55 +0530

    Add files via upload

---
+++ CopyStringintoCharArray.cpp
@@ -0,0 +1,15 @@
+// Program to Copy String into Character Array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	string str="Coding is Life!";
+	char ch[20];
+	int i=0,j=0;
+	while(ch[i++]=str[j++]);
+	cout<<ch;
+
+	return 0;
+}
