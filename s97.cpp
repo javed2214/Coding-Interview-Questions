8597443d0f37a8473ae89d927028b726f8dde50c
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-01-21 21:17:54 +0530

    Add files via upload

---
+++ s97.cpp
@@ -0,0 +1,21 @@
+// Program to Print All Possible Pairs of Array Elements which are Equal to Given Sum
+
+using namespace std;
+
+int main(){
+	
+	int a[]={3,2,5,1,4,7,6,9,10,8};
+	int n=sizeof(a)/sizeof(int);
+	int sum=11;
+
+	map <int,bool> Map;
+	for(int i=0;i<n;i++)
+		Map[a[i]]=true;
+	
+	for(int i=0;i<n/2;i++){
+		if(Map[sum-a[i]]==true){
+			cout<<a[i]<<" "<<sum-a[i]<<"\n";
+		}
+	}
+	return 0;
+}
