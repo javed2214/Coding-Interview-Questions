6cd66c593d809514455f4c3f06368372a969540b
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-02-11 09:12:59 +0530

    Add files via upload

---
+++ Anagram-Pairs.cpp
@@ -0,0 +1,27 @@
+// Program to Find Anagram Pairs
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void anagramPairs(vector<string> &vec){
+
+	map<string,vector<string>> Map;
+	for(int i=0;i<vec.size();i++){
+		string str=vec[i];
+		sort(str.begin(),str.end());
+		Map[str].push_back(vec[i]);
+	}
+	for(auto it:Map){
+		for(auto ptr:it.second)
+			cout<<ptr<<" ";
+		cout<<"\n";
+	}
+}
+
+int main(){
+
+	vector<string> vec={"geeks","quiz","hello","ziuq","eeksg","ellho"};
+	anagramPairs(vec);
+
+	return 0;
+}
