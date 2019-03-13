362cec4ddbee0992fbf4ba4ad8d4eb15e5f3a875
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-03-13 10:25:45 +0530

    Add files via upload

---
+++ Name-Initials.cpp
@@ -0,0 +1,21 @@
+// Program to Find Name Initials
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	string str;
+	getline(cin,str);
+
+	int x=0;
+	for(int i=0;i<str.length();i++){
+		if(str[i]==' '){
+			cout<<str[x]<<" ";
+			x=i+1;
+		}
+		else if(i==str.length()-1)
+			cout<<str.substr(x);
+	}
+	return 0;
+}
