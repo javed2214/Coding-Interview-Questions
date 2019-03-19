241ad109c6aece96e92920191efc1c8a2349c054
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-03-20 01:58:39 +0530

    Add files via upload

---
+++ Corresponding-Characters.cpp
@@ -0,0 +1,15 @@
+// Print Corresponding Characters from End
+// a -> z
+// b -> x and so on...
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	char ch='m';
+	int x=122-(ch-'a');
+	cout<<(char)x;
+
+	return 0;
+}
