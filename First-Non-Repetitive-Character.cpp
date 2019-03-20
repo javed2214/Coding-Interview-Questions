8b59f972d688c0b92d454165e486050f3edafa38
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-03-20 14:15:34 +0530

    Add files via upload

---
+++ First-Non-Repetitive-Character.cpp
@@ -0,0 +1,24 @@
+// Find First Non Repetitive Character of a String
+// Using HashMap 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	string str="abgdefbdaef";
+	int n=str.length();
+
+	int hash[26]={0};
+
+	for(int i=0;i<n;i++)
+		hash[str[i]-'a']++;
+
+	for(int i=0;i<n;i++){
+		if(hash[str[i]-'a']==1){
+			cout<<str[i]<<"\n";
+			break;
+		}
+	}
+	return 0;
+}
