6ab00b96ce490592c0681082510478e4acc19296
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-03-31 11:37:55 +0530

    Add files via upload

---
+++ Lexicographically-Next-String.cpp
@@ -0,0 +1,25 @@
+// Program to Find Lexicographically Next String
+
+#include<bits/stdc++.h>
+using namespace std;
+
+string nextString(string str){
+
+	if(str=="") return "a";
+	int n=str.length()-1;
+	
+	while(str[n]=='z' and n>=0) n-=1;
+
+	if(n==-1) str+='a';
+	else str[n]++;
+
+	return str;
+}
+
+int main(){
+
+	string str="geeks";
+	cout<<nextString(str);
+
+	return 0;
+}
