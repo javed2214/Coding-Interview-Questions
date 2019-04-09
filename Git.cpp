80fa4d650777670b74a3bd0e650f60e19905a5b1
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-04-09 11:36:30 +0530

    Add files via upload

---
+++ Git.cpp
@@ -0,0 +1,31 @@
+// ------- Author: Javed Ansari -------
+// http://codeforces.com/problemset/problem/1141/B
+
+#include<bits/stdc++.h>
+#include<cstring>
+#include<stdio.h>
+using namespace std;
+#define MOD 1000000007
+#define DB(x) cout<<#x<<"="<<x<<endl;
+#define FASTREAD ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define s(a) sort(a.begin(),a.end());
+#define ll long long int
+#define SIZE 100000001
+
+int main(){
+
+	int a[]={1,0,0,1,0,0,1,1,1};
+	int n=sizeof(a)/sizeof(int);
+
+	int sum=0,mx=INT_MIN;
+
+	for(int i=0;i<2*n;i++){
+
+		if(a[i%n]==1) sum+=1;
+		else sum=0;
+		mx=max(mx,sum);
+	}
+	cout<<mx<<"\n";
+
+	return 0;
+}
