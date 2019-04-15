c6ac336902e128d0561437b1aae253d8dcdcb682
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-04-15 20:06:59 +0530

    Add files via upload

---
+++ MACROS.cpp
@@ -0,0 +1,34 @@
+																																																								/*
+**************************
+*  Author: Javed Ansari  *
+**************************
+							                        																																											*/
+#include<bits/stdc++.h>
+#include<cstring>
+#include<stdio.h>
+using namespace std;
+#define MOD1 1000000007
+#define MOD2 1000000009
+#define DB(x) cout<<#x<<"="<<x<<endl;
+#define FASTREAD ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define s(a) sort(a.begin(),a.end())
+#define test(t) ll t; cin>>t; while(t--)
+#define pi 2*acos(0.0)
+#define endl cout<<"\n";
+#define rev(v) reverse(v.begin(),v.end())
+#define sum(v) accumulate(v.begin(),v.end(),0)
+#define maxe(v) *max_element(v.begin(),v.end())
+#define mine(v) *min_element(v.begin(),v.end())
+#define ll long long int
+#define ld long double
+#define SIZE 100000001
+
+
+int main(){
+
+	FASTREAD;
+
+	
+
+	return 0;
+}
