22ab09de8b25437a2170d6fbc62cb3a55e445fd3
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-05-02 00:52:33 +0530

    Add files via upload

---
+++ Longest-Subarray-With-K-Sum.cpp
@@ -0,0 +1,23 @@
+// Longest Subarray With 'K' Sum
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	int a[]={10,5,2,7,1,9};
+	int n=sizeof(a)/sizeof(int);
+	// Pre-Sum: 10 15 17 24 25 34
+
+	int k=15,sum=0,mx=0;
+	unordered_map <int,int> Map;
+
+	for(int i=0;i<n;i++){
+		sum+=a[i];
+		if(Map.find(sum)==Map.end()) Map[sum]=i;
+		if(Map.find(sum-k)!=Map.end()) mx=max(mx,i-Map[sum-k]);
+	}
+	cout<<mx;
+
+	return 0;
+}
