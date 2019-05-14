dc13f157c9b297179eb652e10f5de3eea894d196
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-05-14 18:00:55 +0530

    Add files via upload

---
+++ Integer-Zero.cpp
@@ -0,0 +1,16 @@
+// To Check Whether the Input Integer is 0 or Not Without Using '==' Operator
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main(){
+
+	int n;
+	cin>>n;
+	
+	if(n|0) cout<<"No";
+	else cout<<"Yes";
+
+	return 0;
+}
