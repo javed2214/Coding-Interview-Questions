0046b8758b0b68815ef8cd0c00390207dc0fa999
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-07-29 19:11:07 +0530

    Update Kadane's Algorithm.cpp

--- Kadane's Algorithm.cpp
+++ Kadane's Algorithm.cpp
@@ -1,21 +1,27 @@
-// Kadane's Algorithm (Max Sum Contiguous SubArray)
+// Kadane's Algorithm
+// Maximum Sum Subarray
+// https://www.youtube.com/watch?v=YxuK6A3SvTs&list=LLLmLE3lOJcgWJQe4TjuByaQ&index=5&t=0s
 
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
 
-	int a[]={2,2,-3,7};
-	int n=sizeof(a)/sizeof(int);
+	int a[] = {-4,1,-3,7,-2,5,7,-9,-3};
+	int n  =  sizeof(a)/sizeof(int);
 
-	int max=INT_MIN, sum=0;
-	
-	for(int i=0;i<n;i++){
-		sum+=a[i];
-		if(sum>max) max=sum;
-		if(sum<0) sum=0;
+	int meh = 0, msf = INT_MIN;
+
+	// meh  =  Max Ending Here
+	// msf  =  Max So Far
+
+	for(int i = 0;i<n;i++){
+
+		meh = meh + a[i];
+		meh = max(meh, a[i]);
+		msf = max(msf, meh);
 	}
-	cout<<max;
+	cout<<msf;
 
 	return 0;
 }
