599c0d370072052f5b08d491b9ac3e1276eaff9d
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-08-30 19:59:08 +0530

    Update Segregate-Zeros-and-Ones.cpp

--- Segregate-Zeros-and-Ones.cpp
+++ Segregate-Zeros-and-Ones.cpp
@@ -1,25 +1,52 @@
 // Program to Segregate Zeros and Ones
+// Two Approaches
 
 #include<bits/stdc++.h>
 using namespace std;
 
+void method1(int *a, int n){
+
+	int l = 0;
+	int r = n - 1;
+
+	while(l < r){
+
+		while(a[l] == 0 and l < r) l++;
+		while(a[r] == 1 and l < r) r--;
+
+		if(l < r) swap(a[l], a[r]); 
+	} 
+	for(int i=0;i<n;i++) cout<<a[i]<<" ";
+
+	cout<<endl;
+}
+
+
+void method2(int *a, int n){
+
+	int l = 0;
+	int r = n - 1;
+
+	while(l < r){
+
+		if(a[l] == 1){
+			swap(a[l], a[r]);
+			r -= 1;
+		}
+		else l += 1;
+	}
+	for(int i=0;i<n;i++) cout<<a[i]<<" ";
+}
+
+
 int main(){
 
-	int a[]={1,0,1,0,0,1,1,1,0,1,0,0};
-	int n=sizeof(a)/sizeof(int);
+	int a[] = {1,0,1,0,0,1,1,1,0,1,0,0};
+	int n = sizeof(a)/sizeof(int);
 
-	int l,r;
-	l=0;r=n-1;
+	method1(a,n);
 
-	while(l<r){
-		if(a[l]==1){
-			swap(a[l],a[r]);
-			r--;
-		}
-		else l++;
-	}
-
-	for(auto it:a) cout<<it<<" ";
+	method2(a,n);
 
 	return 0;
 }
