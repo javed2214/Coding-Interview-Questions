923b8341a26dfe7c3fdcec36aba51386cc961423
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-11-09 10:37:47 +0530

    Update Pascals-Triangle.cpp

--- Pascals-Triangle.cpp
+++ Pascals-Triangle.cpp
@@ -1,5 +1,32 @@
 // Program to Print Pascal's Triangle
 
+
+// LeetCode Solution
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        vector<vector<int>> res(numRows);
+        
+        for(int i=0;i<numRows;i++){
+            
+            res[i].resize(i+1);
+            
+            res[i][0] = res[i][i] = 1;
+            
+            for(int j=1;j<i;j++)
+                res[i][j] = res[i-1][j-1] + res[i-1][j];
+        }
+        
+        return res;
+    }
+};
+
+
+
+// Another Method
+
 #include<bits/stdc++.h>
 using namespace std;
 
