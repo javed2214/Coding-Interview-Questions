e909ec868c701d6f8d1c3eb8c0aef802c5f915fc
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-11-09 13:49:48 +0530

    Add files via upload

---
+++ Pascals-Triangle-II-LeetCode.cpp
@@ -0,0 +1,18 @@
+// Pascal's Triangle II LeetCode
+// https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        
+        vector <int> res(rowIndex + 1);
+        
+        res[0] = 1;
+        
+        for(int i=1;i<=rowIndex;i++)
+            for(int j=i;j>=1;j--)
+                res[j] += res[j-1];
+        
+        return res;
+    }
+};
