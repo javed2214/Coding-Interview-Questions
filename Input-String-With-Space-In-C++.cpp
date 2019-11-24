21ab4a7003a583f31b5b6843675ce0f71afcef80
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2019-11-24 23:32:23 +0530

    Add files via upload

---
+++ Input-String-With-Space-In-C++.cpp
@@ -0,0 +1,16 @@
+// Input String With Space in C++
+// Using getline() function
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+	string str;
+
+	getline(cin >> ws, str);
+
+	cout << str;
+
+	return 0;
+}
