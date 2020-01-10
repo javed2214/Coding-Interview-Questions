506c99f95144c8114e29eff5be20d2a258ff354b
Javed Ansari <46120375+javed2214@users.noreply.github.com>
2020-01-10 16:15:39 +0530

    Add files via upload

---
+++ Boats-To-Save-People-LeetCode.cpp
@@ -0,0 +1,20 @@
+// Boats to Save People
+// https://leetcode.com/problems/boats-to-save-people/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        
+        sort(people.begin(), people.end());
+        int n = people.size();
+        
+        int l = 0, r = n-1, cnt = 0;
+        
+        while(l <= r){
+            if(people[l] + people[r] <= limit) ++l;
+            cnt++;
+            --r;
+        }
+        return cnt;
+    }
+};
